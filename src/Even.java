de85e9d701e792f9a323e93e65687ee7291c9a29
siyanakoriya <siyanakoriya122@gmail.com>
2022-08-30 07:57:23 +0530

    Even Position Element

---
+++ src/Even.java
@@ -0,0 +1,17 @@
+
+
+public class Even {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        for (int i = 1; i < arr.length; i++) {
+            if (i%2==0) {
+                System.out.println(arr[i]);
+            }
+        }
+    }
+}
+
+
+
+
+
