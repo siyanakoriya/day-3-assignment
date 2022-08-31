c036b34cccae1636cdb14d911e99f07eb9338dc9
siyanakoriya <siyanakoriya122@gmail.com>
2022-08-31 14:00:43 +0530

    largest element

---
+++ src/Largest.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+public class Largest {
+    public static void main(String[] args) {
+        int a[]=new int [5];
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Array Element ");
+        for(int i=0;i<5;i++) {
+            a[i]=sc.nextInt();
+        }
+        int largest = a[0];
+        for(int i=1;i<5;i++) {
+            if(a[i]>largest) {
+                largest = a[i];
+            }
+        }
+        System.out.println("Largest Element "+largest);
+    }
+}
